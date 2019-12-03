ca46efd4f0fd4ab5814e41b4d3ec69b9d6ee2302
Shyam <m.shyam.tnj@gmail.com>
2019-12-04 00:56:29 +0530

    Update readme

--- AS-files/app/src/main/java/com/github/shyamking/remindeverything/MainActivity.kt
+++ AS-files/app/src/main/java/com/github/shyamking/remindeverything/MainActivity.kt
@@ -3,11 +3,18 @@ package com.github.shyamking.remindeverything
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var sampleText: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sampleText = findViewById(R.id.sampleText)
+
+        sampleText.setText("Hi there")
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # remind-everything-android
 Remind Everything front-end: Android app
+
+## About this repo
+This repo contains the files related to the Android app Remind Everything
+
+## Repo structure
+.
+├── AS-files    <- Android project
+└── README.md	<- You are reading this
